92bee516b1c176fa6bd6d56ee441787f46fd4743
shusaitou <s2422071@stu.musashino-u.ac.jp>
2026-05-19 13:51:19 +0900

    クライアントの初期化を仮実装

--- clearcode/lib/supabase.ts
+++ clearcode/lib/supabase.ts
@@ -1,3 +1,10 @@
 // TODO: Supabaseクライアントの初期化を後で実装
 // 担当: A
-export {};
+import { createClient as supabaseCreateClient } from "@supabase/supabase-js";
+
+export function createClient() {
+  return supabaseCreateClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+}
